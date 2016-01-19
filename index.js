9139b741dcf9a92905121cf9bcee77e4cfdc2ef2
Richard Mable <richardbmable@gmail.com>
2016-01-19 17:55:09 -0500

    creating a next function, doesn't quite work yet

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+window.onload = function(){
 //set the playlist array of original mp3 files
-var playlist = ['mp3file/classical.mp3', 'mp3file/newborn.mp3', 'mp3file/watchtower.mp3', 'mp3file/muse.mp3']
+var playlist = ['mp3file/classical.mp3', 'mp3file/newborn.mp3', 'mp3file/watchtower.mp3', 'mp3file/muse.mp3'];
 //create an empty array to push the JS created audio files into
-var playlistAudio = []
+var playlistAudio = [];
 //set x to zero for use in the for in loop
 x = 0;
 //loop through the playlist of music files
@@ -13,6 +14,10 @@ for (x in playlist) {
 	//increment x by one to move onto the next index of the array
 	x++;
 };
+
+var audio = playlistAudio[0];
+
+var newAudio = playlistAudio[1];
 
 //list the audio element array in console to check if it's working
 console.log(playlistAudio);
@@ -32,52 +37,60 @@ console.log(playlistAudio);
 function Jukebox(song){
 	//method to play the song
 	//pass in the current loaded song with the argument
-	   this.play = function(song){
-		      song.play();
-		      return "Song is now playing";
-	   }
+   this.play = function(song){
+		song.play();
+		return "Song is now playing";
+   }
 	//method to pause the song
 	//pass in the current playing song with the argument
     this.pause = function(song){
 		//use .pause to stop current song
-		      song.pause();
-		      return "Song is now paused.";
+		song.pause();
+		return "Song is now paused.";
+    }
+    //next method to go to the next song in the array
+    this.next = function(){
+    	x = 0
+    	var nextButton = document.getElementById("nextButton");
+    	nextButton.onclick = function(){
+    		x++;
+    	};
+    	audio = playlistAudio[x]
     }
 	//method to load a new song
 	//pass in the song to be loaded with the arugment
-
 	this.newSong = function(song){
 		currentSong = song.load();
 		return "Song is now loaded.";
 	}
 };
 
+var j = new Jukebox(audio);
 //don't create a new jukebox every time song is loaded
 //on start up, intitalize jukebox by looping through an array of song choices
 //so that they are all loaded into the jukebox
-
-var audio = new Audio('mp3file/classical.mp3');
-var j = new Jukebox(audio);
-var newAudio = new Audio('mp3file/muse.mp3');
-
-window.onload = function(){
+	var nextButton = document.getElementById("nextButton");
+    	nextButton.onclick = function(){
+    		j.next();
+    };
 
 	var playButton = document.getElementById("playButton");
 	playButton.onclick = function(){
-		j.play(audio)
+		j.play(audio);
 	};
 
 	var pauseButton = document.getElementById("pauseButton");
 	pauseButton.onclick = function(){
-		j.pause(audio)
+		j.pause(audio);
 	};
 
 	var newSongButton = document.getElementById("loadButton");
 	newSongButton.onclick = function(){
-		j.newSong(newAudio)
+		j.newSong(newAudio);
 	};
 };
 
+//make an array of 50 songs, then real array of 10 songs, pick 4 random songs, if correct is picked, increment points.
 
 
 
