200800ba097b2eb58c200b2e1622bca214a19ff6
Marck <mercercountynj@gmail.com>
2016-01-19 17:00:36 -0500

    basic play and pause structure JS

--- index.js
+++ index.js
@@ -18,59 +18,49 @@ var playlist = new Array('mp3file/classical.mp3', 'mp3file/newborn.mp3', 'mp3fil
 function Jukebox(song){
 	//method to play the song
 	//pass in the current loaded song with the argument
-	this.play = function(song){
-		song.play();
-		return "Song is now playing";
-	}
+	   this.play = function(song){
+		      song.play();
+		      return "Song is now playing";
+	   }
 	//method to pause the song
 	//pass in the current playing song with the argument
-	this.pause = function(song){
+    this.pause = function(song){
 		//use .pause to stop current song
-		song.pause();
-		return "Song is now paused.";
-	}
+		      song.pause();
+		      return "Song is now paused.";
+    }
 	//method to load a new song
 	//pass in the song to be loaded with the arugment
-	this.newSong = function(song){
-		song.load();
-		return "Song is now loaded.";
-	}
+    this.newSong = function(song){
+        song.load();
+		      return "Song is now loaded.";
+	   }
 };
 
 //don't create a new jukebox every time song is loaded
 //on start up, intitalize jukebox by looping through an array of song choices
 //so that they are all loaded into the jukebox
 
-
-
-
 var audio = new Audio('mp3file/classical.mp3');
 var j = new Jukebox(audio);
 var newAudio = new Audio('mp3file/muse.mp3');
 
 window.onload = function(){
-	var playButton = document.getElementById("playButton");
-	playButton.onclick = function(){
-		j.play(audio)
-	};
+	   var playButton = document.getElementById("playButton");
+	   playButton.onclick = function(){
+		      j.play(audio)
+	   };
 	
-	var pauseButton = document.getElementById("pauseButton");
-	pauseButton.onclick = function(){
-		j.pause(audio)
-	};
+	   var pauseButton = document.getElementById("pauseButton");
+	   pauseButton.onclick = function(){
+		      j.pause(audio)
+	   };
 
-	var newSongButton = document.getElementById("loadButton");
-	newSongButton.onclick = function(){
-		j.newSong(newAudio)
-	};
+	   var newSongButton = document.getElementById("loadButton");
+	   newSongButton.onclick = function(){
+		      j.newSong(newAudio)
+	   };
 
-	// var pauseButton = document.getElementById("pauseButton");
-	// 	pauseButton.addEventListener("click", j.pause(audio));
-
-	// var newSongButton = document.getElementById("loadButton");
-	// 	newSongButton.addEventListener("click", j.newSong(newAudio));
-
-// when button clicked: j.play(currnet song)
 };
 
 
