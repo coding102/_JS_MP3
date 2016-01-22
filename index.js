54fc0ec3c3fd8fffa97c028b092b7613d1112b2e
Richard Mable <richardbmable@gmail.com>
2016-01-22 11:41:16 -0500

    added 10 new songs and created a next and back button. The jukebox now passes in an array of songs that act as a playlist , and then users can cycle through those songs. Pause button also works.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 //set the playlist array of original mp3 files
-var playlist = ['mp3file/classical.mp3', 'mp3file/newborn.mp3', 'mp3file/watchtower.mp3', 'mp3file/muse.mp3'];
+var playlist = ['mp3file/breakingUp.mp3', 'mp3file/dayTrippin.mp3', 'mp3file/disarmYou.mp3', 'mp3file/neverSleepAlone.mp3', 'mp3file/phoenix.mp3', 'mp3file/promise.mp3', 'mp3file/tearDownTheseWalls.mp3', 'mp3file/weDontStop.mp3', 'mp3file/whatever.mp3', 'mp3file/whereAreYouNow.mp3'];
 //create an empty array to push the JS created audio files into
 var playlistAudio = [];
 //set x to zero for use in the for in loop
@@ -15,64 +15,91 @@ for (x in playlist) {
 	x++;
 };
 
-var audio = playlistAudio[0];
-
-var newAudio = playlistAudio[1];
-
 //list the audio element array in console to check if it's working
 console.log(playlistAudio);
 
- // audio.addEventListener('ended', function () {
- //     i = ++i < playlist.length ? i : 0;
- //     console.log(i)
- //     audio.src = playlist[i];
- //     audio.play();
- // }, true);
- // audio.volume = 0.3;
- // audio.loop = false;
- // audio.src = playlist[0];
- // audio.play();
 //this is the object jukebox which has methods to play, stop and load a new song
-
-function Jukebox(song){
+//pass in the playlist you would like to load
+function Jukebox(playlist){
+	console.log(playlist);
+	//check the length of the playlist
+	playListLength = playlist.length;
+	console.log(playListLength)
+	//initialize the currentSong to the first one in the playlist
+	currentSong = playlist[0]
 	//method to play the song
 	//pass in the current loaded song with the argument
-   this.play = function(song){
-		song.play();
-		return "Song is now playing";
+   this.play = function(){
+		currentSong.play();
    }
 	//method to pause the song
 	//pass in the current playing song with the argument
-    this.pause = function(song){
+    this.pause = function(){
 		//use .pause to stop current song
-		song.pause();
-		return "Song is now paused.";
+		currentSong.pause();
     }
     //next method to go to the next song in the array
+    //set the counter to zero outside the method
+    songCounter = 0;
     this.next = function(){
-    	x = 0
-    	var nextButton = document.getElementById("nextButton");
-    	nextButton.onclick = function(){
-    		x++;
+		//check if the song counter is greater than the playlist length
+		if (songCounter == playListLength - 1){
+		songCounter = 0;
+		};
+		//stop the current playing song
+		currentSong.pause();
+		//increment the song counter by one
+		songCounter = songCounter + 1;
+		console.log(songCounter);
+		//set the current song to the next song in the the playlist array via songCounter
+		currentSong = playlist[songCounter];
+		console.log(playlist[songCounter]);
+		//play the song once it gets to the next song so
+		//the user does not have to hit the play button again
+		currentSong.play();
+		//return song counter for use in other methods
+		songCounter;
+	    };
+
+    this.back = function() {
+    	//songCounter at start of back
+    	console.log(songCounter)
+    	//pause the current song
+    	currentSong.pause();
+    	//subtract 1 from the songCounter to go back 1 song
+    	songCounter = songCounter -1
+    	//check to make sure it is not a neagtive number
+    	//if it is, go the back to the end of the playlist
+    	if (songCounter < 0){
+    		songCounter = 9
     	};
-    	audio = playlistAudio[x]
-    }
-	//method to load a new song
+    	//set the currentSong to the corresponding place in the playlist array
+    	currentSong = playlist[songCounter];
+    	//logging to check
+		console.log(playlist[songCounter]);
+		//play the newly set song
+		currentSong.play();
+		//return song counter for use in other methods
+		songCounter;
+    };
+	//method to load a new playlist
 	//pass in the song to be loaded with the arugment
-	this.newSong = function(song){
+	this.newPlaylist = function(){
 		currentSong = song.load();
-		return "Song is now loaded.";
-	}
+	};
+
 };
 
-//create a song object
 
-//then create the playlist object
-
-var j = new Jukebox(audio);
+//create the jukebox object and pass in the current playlist
+var j = new Jukebox(playlistAudio);
 //don't create a new jukebox every time song is loaded
 //on start up, intitalize jukebox by looping through an array of song choices
 //so that they are all loaded into the jukebox
+	var backButton = document.getElementById("backButton");
+	backButton.onclick = function(){
+		j.back();
+	};
 	var nextButton = document.getElementById("nextButton");
     	nextButton.onclick = function(){
     		j.next();
@@ -80,17 +107,12 @@ var j = new Jukebox(audio);
 
 	var playButton = document.getElementById("playButton");
 	playButton.onclick = function(){
-		j.play(audio);
+		j.play();
 	};
 
 	var pauseButton = document.getElementById("pauseButton");
 	pauseButton.onclick = function(){
-		j.pause(audio);
-	};
-
-	var newSongButton = document.getElementById("loadButton");
-	newSongButton.onclick = function(){
-		j.newSong(newAudio);
+		j.pause();
 	};
 };
 
